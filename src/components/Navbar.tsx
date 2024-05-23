4863cd1903bea303eb370b622ff2a4943a3b4ac2
Douglascrc <douglasbuscam@gmail.com>
2024-05-23 20:34:38 -0300

    feat: use the Hook 'useNavigate' and 'Link'

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,21 +1,30 @@
 import React from 'react';
 import Logo from '@/assets/logo.svg';
+import { Link, useNavigate } from 'react-router-dom';
 
 
 const Navbar = () => {
+
+  const _navigate = useNavigate()
     return ( 
      <>
-        <header className="flex bg-gray-400 items-center justify-between py-1 px-1 backdrop-blur-lg">
+        <header className="flex bg-white bg-opacity-30 items-center justify-between py-1 px-1 backdrop-blur-lg">
+           <Link to='/dashboard' className='items-center justify-center flex cursor-pointer'>
             <img src={Logo} alt="Logo" className="w-12 h-12 ml-16"/>
+            <h1 className='font-normal text-white'>BootPlay</h1>
+           </Link>
+            
             <ul className="flex items-center gap-4 mr-16">
              <li>
-              <button className="bg-black text-white px-4 py-2 rounded-2xl">
-                <a href="/login">Entrar</a> 
+              <button onClick={() => _navigate('/login')}
+               className="bg-black text-white px-4 py-2 rounded-2xl">
+                Entrar
               </button>
              </li>
              <li>
-              <button className="bg-blue-300 text-black px-4 py-2 rounded-2xl"> 
-                <a href="/signup">Inscrever-se</a>  
+              <button onClick={ () => _navigate('/signup')}
+               className="bg-blue-300 text-black px-4 py-2 rounded-2xl"> 
+                Inscrever-se  
               </button>
              </li>
             </ul>
