216d7db8b7f7f14f1e1fd55b649a6a703cdd43c9
Douglascrc <douglasbuscam@gmail.com>
2024-05-26 20:53:47 -0300

    refactor: Update Input component styles

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -14,7 +14,7 @@ export default function Input({ children, type, required, hig, onChange } : Prop
     <>
       <label className={`${hig && "text-green-600"} flex flex-col w-full text-sm font-normal text-zinc-400 mb-1`}>
         {children}
-        <input type={type} required={required} onChange={onChange} className="w-full ring-1 ring-zinc-400 h-8 text-zinc-500 font-semibold rounded-md hover:ring-blue-400" />
+        <input type={type} required={required} onChange={onChange} className="w-full ring-1 p-2 h-10 text-zinc-500 font-semibold rounded-md hover:ring-blue-400" />
       </label>
     </>
   )
