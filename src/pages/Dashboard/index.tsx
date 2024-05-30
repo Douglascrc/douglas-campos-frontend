6eb536af22033d463fef4a7daae6ddc432cfeb54
Douglascrc <douglasbuscam@gmail.com>
2024-05-30 18:45:29 -0300

    feat: Add lazy loading for images and update Dashboard styles

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,32 +1,72 @@
 import { AlbumModel } from '@/models/AlbumModel';
 import { album_api } from '@/services/apiService';
 import React, { useEffect, useState } from 'react'
+import { Card } from '@/components/ui/card';
+import { FiSearch } from 'react-icons/fi';
+import Navbar from '@/components/Navbar';
 
 const Dashboard = () =>  {
   const [albums, setAlbums] = useState<AlbumModel[]>([]);
+  const [search, setSearch] = useState<string>('');
 
-  useEffect(() => {
-    album_api.defaults.headers.common.Authorization = "Basic YWx2ZXNyZW5hbnNhbnRvc0Bob3RtYWlsLmNvbTokMmEkMTAkZU5vblNhaDZHMGEvNnA2T2ZOVk1nZWUxRVlOY2RWOUZHQm5OaHp0azJhYU9odlg3OS80Slc"
-    album_api.get('/albums/all?searchText=Rock')
+  const fetchAlbums = () => {
+
+    album_api.defaults.headers.common.Authorization = `Basic ${localStorage.getItem('@Auth.Token')}`;
+
+    if(search === '') {
+     return null 
+    }
+    album_api.get(`/all?searchText=${search}`)
     .then((resp) => {
       setAlbums(resp.data);
       console.log(albums);
     })
-  }, []);
+  };
 
+  useEffect(() => {
+    fetchAlbums();
+  },[])
 
   function handleLink(url?: string) {
     window.open(url, '_blank');
   }
 
   return (
-    <main className="flex flex-col items-center justify-center h-full mt-10 gap-4">
-      <h1 className="text-2xl font-semibold">Albums</h1>
-      <input type="text" />
-      <button>Buscar</button>
+    
+    <div className="flex flex-col min-h-screen bg-[#19181F]">
+      
+      <nav>
+        
+      </nav>
 
-      <section className="flex flex-wrap justify-center h-full gap-4 m-2">
-        {/* Card */}
+      <div className='flex h-[70vh] relative bg-profile bg-size-person bg-pos-custom bg-no-repeat' >
+        <div className="w-full h-full absolute bg-gradient-to-t from-[#19181F] to-10%"></div>
+
+        <main className='flex flex-col w-full h-full bg-opacity-50 bg-neutral-950 '>
+          <Navbar/>
+          <section className='flex flex-col lg:w-[35%] w-[80%] justify-center mt-56 drop-shadow-[-1px_1px_0_rgb(0,0,0)] p-4 gap-8'>
+            <h1 className='text-white font-semibold text-4xl'> A história da música não pode ser esquecida!</h1>
+            <span className='text-white font-normal text-xl' > Sucessos que marcaram o tempo!!!</span>  
+          </section>    
+        </main>
+      </div>
+      
+      
+      <div className='flex flex-row w-fit items-center justify-center gap-2 mx-auto my-2'>
+        <input  name="search"
+                  type="text"
+                  className="bg-[#19181F] ring-1 ring-white text-white hover:ring-2 focus:ring-2 w-full focus:outline-none rounded-md p-2 pr-14"
+                  placeholder="Procure algo..." onChange={(e) => setSearch(e.target.value)}/>
+                  <button type='submit' onClick={fetchAlbums}>
+                    <FiSearch className='w-8 h-8'/>
+                  </button>
+      </div>
+
+      <div className='justify-start ml-4'>
+        <h1 className="text-2xl font-semibold text-white">Trends</h1>
+      </div>
+      <Card className="flex flex-wrap justify-center h-full gap-4 border-none m-2">
+       
         { albums?.map((album, i) => (
           <div key={i} style={{'--bg-fundo': `url(${album.images[0].url})`} as React.CSSProperties} className="bg-[image:var(--bg-fundo)] bg-cover bg-no-repeat w-60 h-[245px] rounded-md">
             <div onClick={() => handleLink(album.externalUrls.externalUrls.spotify)} className="flex h-full justify-center items-center backdrop-brightness-50 p-6 cursor-pointer">
@@ -34,20 +74,9 @@ const Dashboard = () =>  {
             </div>
           </div>
         ))}
-
-        {/* { albums?.map((album, i) => (
-          <div className="w-60 h-[245px] rounded-md">
-            <div className="absolute w-60 h-[245px] backdrop-brightness-50">
-              <div className="flex items-center justify-center w-full h-full p-6">
-                <h1 className="text-2xl font-semibold text-center text-white">{album.name}</h1>
-              </div>
-            </div>
-            <img src={album.images[0].url} alt={album.name} className="w-full h-60 object-cover" />
-          </div>
-        )) } */}
-        {/* Card */}
-      </section>
-    </main>
+      </Card>
+    </div>
+    
   )
 }
 
