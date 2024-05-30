4b4d500f981e27f2501a608cbb766ac3c9fb8cb5
Douglascrc <douglasbuscam@gmail.com>
2024-05-30 18:45:58 -0300

    Update API endpoints for album and user APIs

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -5,9 +5,9 @@ export const api = axios.create({
 });
 
 export const album_api = axios.create({
-  baseURL: 'http://localhost:8082/api'
+  baseURL: 'http://localhost:8082/api/albums'
 });
 
 export const user_api = axios.create({
-  baseURL: 'http://localhost:8081/api'
+  baseURL: 'http://localhost:8081/api/users'
 });
