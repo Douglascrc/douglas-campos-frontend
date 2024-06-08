da1fb7c1dc6c61995a3dc69b99043a81b8f62b54
Douglascrc <douglasbuscam@gmail.com>
2024-06-08 19:19:11 -0300

    refactor: Update MyDisks and add svg images

--- src/pages/MyDisks/index.tsx
+++ src/pages/MyDisks/index.tsx
@@ -1,60 +1,59 @@
 import { AlbumModel } from '@/models/AlbumModel';
 import { album_api } from '@/services/apiService';
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 import './style.css';
-
-const Albums = [
-  { name: 'Album01', image: 'https://picsum.photos/200', url: '' },
-  { name: 'Album02', image: 'https://picsum.photos/200', url: '' },
-  { name: 'Album03', image: 'https://picsum.photos/200', url: '' },
-  { name: 'Album04', image: 'https://picsum.photos/200', url: '' },
-  { name: 'Album05', image: 'https://picsum.photos/200', url: '' },
-  { name: 'Album06', image: 'https://picsum.photos/200', url: '' },
-  { name: 'Album07', image: 'https://picsum.photos/200', url: '' },
-  { name: 'Album08', image: 'https://picsum.photos/200', url: '' },
-  { name: 'Album09', image: 'https://picsum.photos/200', url: '' },
-  { name: 'Album10', image: 'https://picsum.photos/200', url: '' }
-]
+import Navbar from '@/components/Navbar';
 
 
 const MyDisks = () => {
   const [albums, setAlbums] = useState<AlbumModel[]>([]);
-  const _navegate = useNavigate();
+  const [investment, setInvestment] = useState(Number)
+  const [totalAlbums, setTotalAlbums] = useState(Number)
 
   useEffect(() => {  
-    album_api.get('/all?SearchText=Rock', {headers: { Authorization: `Basic ${localStorage.getItem('@Auth.Token')}`}})
-    .then((resp) => {
-      setAlbums(resp.data);
-      console.log(albums);
+    album_api.defaults.headers.common.Authorization = `Basic ${localStorage.getItem('@Auth.Token')}`
+    album_api.get('/my-collection').then(resp => {
+      const albums = resp.data
+      setAlbums(albums)
+
+      const qtdAlbums = albums.length
+      setTotalAlbums(qtdAlbums)
+
+      const value = albums.reduce((acc: number, album: { value: number; }) => acc + album.value, 0)
+      const valueFormated = value.toFixed(2).replace('.', ',')
+      setInvestment(valueFormated)
+      console.log(resp)
     })
+    
   }, []);
 
   return (
-    <section className="flex flex-wrap justify-center h-full gap-4 m-2">
-      {/* Card */}
-      { Albums?.map((album, i) => (
-        <div key={i} style={{'--bg-fundo': `url(${album.image})`} as React.CSSProperties} className="bg-[image:var(--bg-fundo)] bg-cover bg-no-repeat w-60 h-[245px] rounded-md">
-          <div onClick={() => _navegate(album.url)} className="flex h-full justify-center items-center backdrop-brightness-50 p-6 cursor-pointer">
-            <h1 className="text-2xl font-semibold text-center text-white">{album.name}</h1>
-          </div>
-        </div>
-      ))}
-      <div className="flex items-center justify-center h-screen relative overflow-hidden">
-       <div className="carousel-dashboard absolute left-0 flex items-center w-full">
-         {Albums.map((album, i) =>(
-          <div className="pr-8">
-            <div key={i} style={{'--bg-fundo': `url(${album.image})`} as React.CSSProperties} className="bg-[image:var(--bg-fundo)] bg-cover bg-no-repeat w-60 h-[245px] rounded-md hover:scale-110 transition">
-              <div onClick={() => _navegate(album.url)} className="flex h-full justify-center items-center backdrop-brightness-50 p-6 cursor-pointer">
-                <h1 className="text-2xl font-semibold text-center text-white">{album.name}</h1>
-              </div>
-            </div>
-          </div>
-        ))}
+    <main className=" h-screen bg-[#19181F] ">
+      <Navbar/>
+      <div className="pt-44 pl-12 MyDiscs">
+        <h1 className=" font-bold text-white " style={{fontSize: '35px'}}>Meus Discos</h1>
       </div>
-    </div>
-    </section>
-    
+
+      <section className="pl-12 mt-8 flex invest ">
+        <div className="w-60 p-3 rounded-lg flex bg-white" style={{border: '1px solid #000'}}>
+          <img className="w-10 h-10 p-2 rounded-full bg-black" src="./src/assets/iconVideo.svg"/>
+          <h1 className="ml-3 text-black font-bold">Total de Albuns   <br/> <h1 className="font-normal" style={{fontSize: '20px'}}>{totalAlbums}</h1></h1>
+        </div>
+
+        <div className="w-60 p-3 rounded-lg h-full bg-white flex ml-5" style={{border: '1px solid #000'}}>
+          <img className="w-10 h-10 rounded-full bg-black p-2" src="./src/assets/real.svg"/>
+          <h1 className="ml-3 text-black font-bold">Valor Investido   <br/> <h1 className="font-normal" style={{fontSize: '20px'}}>R$ {investment}</h1></h1>
+        </div>
+      </section >  
+
+      <div className="mt-10 flex flex-wrap pl-10 pb-10 gap-4 albumsMobile">
+        { albums?.map((album, i) => (
+            <div key={i} style={{backgroundImage:`url(${album.images})`}} className="bg-cover  bg-no-repeat w-60 h-[245px] rounded-md">                                       
+            </div>
+                  ))}
+            </div>                                               
+    </main>
   )
 }
 export default MyDisks;
