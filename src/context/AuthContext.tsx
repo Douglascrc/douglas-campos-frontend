3f151f4cf9d388887e59273c15bb467c71f4e3a9
Douglascrc <douglasbuscam@gmail.com>
2024-06-18 09:53:49 -0300

    refactor: Remove unnecessary Logout() call in AuthProvider

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -25,7 +25,6 @@ export const AuthProvider: React.FC<Props> = ({children}) => {
       setIsAuthenticated(true);
       setUserData(data);
     }
-    Logout();
   }, []);
 
   const Login = useCallback(async (email: string, password: string) => {
