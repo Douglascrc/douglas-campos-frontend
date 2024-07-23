6dbdc52a535ef1a3f43df350c2d665541021df74
Douglascrc <douglasbuscam@gmail.com>
2024-07-23 15:47:18 -0300

    chore: ensures eslint pattern

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 
 interface Props {
   children: React.ReactNode;
@@ -10,12 +10,12 @@ interface Props {
 }
 
 export default function Input({ children, type, required, hig, onChange } : Props) {
-  return (
-    <>
-      <label className={`${hig && "text-green-600"} flex flex-col w-full text-sm font-normal text-zinc-400 mb-1`}>
-        {children}
-        <input type={type} required={required} onChange={onChange} className="w-full ring-1 p-2 h-10 text-zinc-500 font-semibold rounded-md hover:ring-blue-400" />
-      </label>
-    </>
-  )
+ return (
+  <>
+   <label className={`${hig && 'text-green-600'} flex flex-col w-full text-sm font-normal text-zinc-400 mb-1`}>
+    {children}
+    <input type={type} required={required} onChange={onChange} className="w-full ring-1 p-2 h-10 text-zinc-500 font-semibold rounded-md hover:ring-blue-400" />
+   </label>
+  </>
+ );
 }
